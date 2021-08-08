71a0d372d1a56b2bc870ac5a54a9a0e182cc2e62
simon.syga@web.de <simon.syga@web.de>
2021-08-08 12:45:45 +0200

    final commit

--- randomnetworkmodel.py
+++ randomnetworkmodel.py
@@ -4,6 +4,7 @@ from scipy.spatial import Delaunay
 from numpy.random import random
 from numpy.linalg import norm
 from matplotlib import pyplot as plt
+from itertools import combinations
 
 
 def get_rwpoints(n, c, r):
@@ -22,6 +23,40 @@ def get_rwpoints(n, c, r):
 
     x, y = xc[..., None] + r * np.cos(phi), yc[..., None] + r * np.sin(phi)
     return np.array((x.flatten(), y.flatten())).T
+
+def rotmatrix(theta):
+    c, s = np.cos(theta), np.sin(theta)
+    R = np.array(((c, -s), (s, c)))
+    return R
+
+
+# def get_sppoints(n, c, r):
+#     """
+#     Return the positions of n * c points, so that there are n points randomly placed around c centers in a r x r grid.
+#     The centers are placed randomly on a circle with radius sqrt(c).
+#     :param n: number of nodes around a center
+#     :param c: number of centers
+#     :param r: radius of circle around center
+#     :return: array of shape (n * c, 2) containing the positions of the points
+#     """
+#     rc = np.sqrt(c)  # radius for centers
+#     phic = 2 * np.pi * random(c)
+#     # rcs = np.linspace(0, rc, c, endpoint=True)
+#     xc, yc = rc * np.cos(phic), rc * np.sin(phic)
+#     phi = 2 * np.pi * random(c)  # rotation angles of grids
+#     rs = np.linspace(0, r, n, endpoint=True)  # + r * random(n)
+#     x, y = np.mgrid[-1:1:complex(0, n), -1:1:complex(0, n)] * r
+#     x, y = x.flatten(), y.flatten()
+#     ind = np.random.randint(len(x), size=n)
+#     points = x[ind], y[ind]
+#     points = np.dot(rotmatrix(phi))
+#
+#     x, y = xc[..., None] + rs * np.cos(phi), yc[..., None] + rs * np.sin(phi)
+#     return np.array((x.flatten(), y.flatten())).T
+
+#
+# def get_gridnw(n, c, r):
+#     return get_Gabriel_graph(get_sppoints(n, c, r))
 
 
 def get_Gabriel_graph(points):
@@ -58,19 +93,31 @@ def get_randomnetwork(n, c, r):
     return get_Gabriel_graph(get_rwpoints(n, c, r))
 
 
+def set_edge_att(G):
+    bc, bci = betweenness_impact(G)
+    nx.set_edge_attributes(G, bc, name='betweenness centrality')
+    nx.set_edge_attributes(G, bci, name='betweenness impact')
+
+
+def distances(G, weight='weight'):
+    ldict = dict(nx.all_pairs_dijkstra_path_length(G, weight=weight))
+    distances = [ldict[u][v] for u, v in combinations(G.nodes(), 2)]
+    return distances
+
+
 def betweenness_impact(G, weight='weight'):
     """
     :param G:
     :return: dict of betweenness centrality impacts
     """
-    bc = list(nx.edge_betweenness_centrality(G, weight=weight).values())
-    meanbc = sum(bc) / len(bc)
+    bc = nx.edge_betweenness_centrality(G, weight=weight)
+    bcv = list(bc.values())
+    meanbc = sum(bcv) / len(bcv)
     bci = {}
     Gtemp = G.copy(as_view=False)
     for u, v, d in G.edges(data=True):
         Gtemp.remove_edge(u, v)
-        # edges = [(x, y, d['weight']) for x, y, d in G.edges(data=True) if (x, y) != (u, v)]
-        # Gtemp.add_weighted_edges_from(edges)
+
         gGe = np.mean(list(nx.edge_betweenness_centrality(Gtemp, weight=weight).values()))
         delta1 = 1 - gGe / meanbc
 
@@ -78,6 +125,29 @@ def betweenness_impact(G, weight='weight'):
         Gtemp.add_edge(u, v, data=d)
 
     return bc, bci
+
+# def betweenness_impact2(G, weight='weight'):
+#     """
+#     :param G:
+#     :return: dict of betweenness centrality impacts
+#     """
+#     bc = nx.edge_betweenness_centrality(G, weight=weight, normalized=False)
+#     bcv = list(bc.values())
+#     N = nx.number_of_nodes(G)
+#     meanbc = sum(bcv) / (N-1) / (N-2)
+#     bci = {}
+#     Gtemp = G.copy(as_view=False)
+#     for u, v, d in G.edges(data=True):
+#         Gtemp.remove_edge(u, v)
+#         # edges = [(x, y, d['weight']) for x, y, d in G.edges(data=True) if (x, y) != (u, v)]
+#         # Gtemp.add_weighted_edges_from(edges)
+#         gGe = np.mean(list(nx.edge_betweenness_centrality(Gtemp, weight=weight).values()))
+#         delta1 = 1 - gGe / meanbc
+#
+#         bci.update({(u, v): delta1})
+#         Gtemp.add_edge(u, v, data=d)
+#
+#     return bc, bci
 
 def edge_impact(G, weight='weight'):
     """
